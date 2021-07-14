7b3c2735d049311c008e3e995eebcf881886e3a4
virzs <2369310227@qq.com>
2021-07-14 14:59:58 +0800

    fix

--- lib/components/Button/Button.d.ts
+++ lib/components/Button/Button.d.ts
@@ -1,3 +1,4 @@
 import React from "react";
 import { ButtonProps } from "./interface";
-export declare const Button: React.FC<ButtonProps>;
+declare const Button: React.FC<ButtonProps>;
+export default Button;
