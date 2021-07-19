74d170d40a1a670c607efbf4123e48d2c5792a0c
virzs <2369310227@qq.com>
2021-07-19 10:13:10 +0800

    feat components spin bounce

--- src/components/Spin/index.tsx
+++ src/components/Spin/index.tsx
@@ -5,6 +5,7 @@
  * @Last Modified time: 2021-07-18 21:57:55
  */
 
+import Bounce from './Bounce';
 import Chase from './Chase';
 import InternalSpin from './Spin';
 import Spinner from './Spinner';
@@ -14,11 +15,13 @@ type InternalSpinType = typeof InternalSpin;
 interface SpinInterface extends InternalSpinType {
   Spinner: typeof Spinner;
   Chase: typeof Chase;
+  Bounce: typeof Bounce;
 }
 
 const Spin = InternalSpin as SpinInterface;
 
 Spin.Spinner = Spinner;
 Spin.Chase = Chase;
+Spin.Bounce = Bounce;
 
 export { Spin };
