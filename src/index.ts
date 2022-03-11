70dcf9a2f28e5bc437f576d56f63914b4434f9d0
virzs <2369310227@qq.com>
2022-03-11 10:06:18 +0800

    :coffin: del 无用代码

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './components/Button';
-export * from './components/Typography';
-export * from './components/Spin';
-export * from './components/ScrollSnap';
-export * from './components/Progress';
