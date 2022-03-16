8fa74e847284574ba038046f1ea78905b48c87e3
virzs <2369310227@qq.com>
2022-03-16 14:13:29 +0800

    :sparkles: feat button

---
+++ packages/core/src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: Vir
+ * @Date: 2022-03-16 14:12:30
+ * @Last Modified by: Vir
+ * @Last Modified time: 2022-03-16 14:12:54
+ */
+
+import React from "react";
+
+const Button = () => {
+  return <button></button>;
+};
+
+export default Button;
