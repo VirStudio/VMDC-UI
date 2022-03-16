de6f23777af6630c9dcd7accbdb0ed7bc1200e92
virzs <2369310227@qq.com>
2022-03-16 14:21:01 +0800

    :necktie: chore button test

--- packages/core/src/components/Button/index.tsx
+++ packages/core/src/components/Button/index.tsx
@@ -2,12 +2,12 @@
  * @Author: Vir
  * @Date: 2022-03-16 14:12:30
  * @Last Modified by: Vir
- * @Last Modified time: 2022-03-16 14:12:54
+ * @Last Modified time: 2022-03-16 14:20:34
  */
 
 import React from "react";
 
-const Button = () => {
+const Button = (props) => {
   return <button></button>;
 };
 
